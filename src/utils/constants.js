3d9719a771695519e781226691712a672a340fd0
Yogesh Basavaraju <w1853885@westminster.ac.uk>
2025-04-03 02:56:52 +0530

    base url modified

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,3 @@
-export const BASE_URL = "http://localhost:9999";
+// export const BASE_URL = "http://localhost:9999";
+
+export const BASE_URL = location.hostname === "hostname" ? "http://localhost:7777" : "/api";
